11afacac5f4fb93859eaded0256d8d63e9345c87
ofcbj <hyojepapa@gmail.com>
2023-08-24 01:59:49 +0900

    다양한 구현

--- bot.py
+++ bot.py
@@ -1,114 +1,23 @@
-import time
-from datetime import datetime
+import re
 import discord
- 
+
+import over
+
+
 TOKEN = 'MTE0MzE0NjM3NzQwNTI3NjI3Mw.GZhA6a.2-lmFpbAOjYak9izAo9iSj1xJASrMMD8Lmxluo'
- 
-table = {'우월코드 데미지 증가': [10, 1,  10, [9.54,  10.94, 12.34, 13.75, 15.15, 16.55, 17.95, 19.35, 20.75, 22.15, 23.56, 24.96, 26.36, 27.76, 29.16]],
-         '명중률 증가':          [12, 11, 22, [4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]],
-         '최대 장탄수 증가':     [12, 23, 34, [27.84, 31.95, 36.06, 40.17, 44.28, 48.39, 52.50, 56.50, 60.71, 64.82, 68.93, 73.04, 77.15, 81.26, 85.37]],
-         '공격력 증가':         [10, 35, 44, [4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]],
-         '차지 데미지 증가':    [12, 45, 56, [4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]],
-         '차지 속도 증가':      [12, 57, 68, [1.98,  2.28,  2.57,  2.86,  3.16,  3.45,  3.75,  4.04,  4.33,  4.63,  4.92,  5.21,  5.51,  5.80,  6.09 ]],
-         '크리티컬 데미지 증가': [12, 69, 80, [6.64,  7.62,  8.60,  9.58,  10.56, 11.54, 12.52, 13.50, 14.48, 15.46, 16.44, 17.42, 18.40, 19.38, 20.36]],
-         '크리티컬 확률 증가':   [10, 81, 90, [2.30,  2.64,  2.98,  3.32,  3.66,  4.00,  4.35,  4.69,  5.03,  5.37,  5.70,  6.05,  6.39,  6.73,  7.07 ]],
-         '방어력 증가':          [10, 91, 100,[4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]]}
-         
-import random, sys
-from operator import itemgetter, attrgetter
 
-def getRatioIndex():
-    howProb = random.randint(0, 99)
-    getIdx = 0
-    if howProb < 60:
-        getIdx = int(howProb/12)
-    elif howProb < 95:
-        getIdx = 5+int((howProb-60)/7)
-    else:
-        getIdx = 10+howProb-95
+dAccount = {}
 
-    return getIdx
+def getTopAccount(nikke):
+    topScore = -1
+    topAccount = None
+    for name, account in dAccount.items():
+        curScore = account.dNikke[nikke].score()
+        if curScore > topScore:
+            topAccount = account
+            topScore = curScore
 
-def getOption(prob, obtained):
-    if random.randint(1, 100) <= prob:
-        while True:
-            whichProb = random.randint(1, 100)            
-            for k, v in table.items():
-                if whichProb >= v[1] and whichProb <= v[2]:
-                    break
-
-            if k not in obtained:
-                break
-            
-        idx = getRatioIndex()
-        ratio = v[3][idx]
-        obtained.append(k)
-        return obtained, k, ratio, idx
-    else:
-        obtained.append('효과 미획득')
-        return obtained, '효과 미획득', -1, -1
-
-dUserModule = {}
-dUserObtained = {}
-dUserLastAccess = {}
-
-def clamp(num, min, max):
-    return min if num < min else max if num > max else num
-
-def processModulel(account):
-    if account not in dUserModule:
-        dUserModule[account] = 10
-    else:
-        dUserModule[account] = clamp(dUserModule[account] + int((datetime.now() - dUserLastAccess[account]).seconds/60), 0, 10)
-    
-    if dUserModule[account] > 0:
-        dUserLastAccess[account] = datetime.now()
-        dUserModule[account] = dUserModule[account]-1
-        return True, 0
-    else:
-        return False, 60-(datetime.now() - dUserLastAccess[account]).seconds
-
-def over(account):
-    success, timeToCharge = processModulel(account)
-    if success:
-        out = ''
-        obtained = []
-        for prob in [100, 50, 30]:
-            obtained, name, ratio, idx = getOption(prob, obtained)
-            if name == '효과 미획득':
-                out += name+'\n'
-            else:
-                out += '[%s] %.2f'%(name, ratio)+'%'+' (%dLv)'%(idx+1)+'\n'
-
-        dUserObtained[account] = obtained
-        #out += '------------------------------\n'
-        out += '남은 커스텀 모듈 갯수: %d'%(dUserModule[account])
-        return out
-    else:
-        return '커스텀 모듈 갯수가 부족합니다.\n커스텀 모듈은 1분에 1개씩 충전됩니다. 남은시간 %d초'%(timeToCharge)
-
-def cali(account):
-    if account in dUserObtained:    
-        success, timeToCharge = processModulel(account)
-        if success:
-            out = ''
-            obtained = dUserObtained[account]
-            for name in obtained:
-                if name == '효과 미획득':
-                    out += name+'\n'
-                else:
-                    idx = getRatioIndex()
-                    ratio = table[name][3][idx]
-                    out += '[%s] %.2f'%(name, ratio)+'%'+' (%dLv)'%(idx+1)+'\n'
-
-            #out += '------------------------------\n'
-            out += '남은 커스텀 모듈 갯수: %d'%(dUserModule[account])
-            return out
-        else:
-            return '커스텀 모듈 갯수가 부족합니다.\n커스텀 모듈은 1분에 1개씩 충전됩니다. 남은시간 %d초'%(timeToCharge)
-    else:
-        return '먼저 오버 명령어를 사용해서 오버로드를 해주세요.'
-
+    return topAccount
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -119,17 +28,59 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
  
-        command = message.content.replace(" ", "")
+        #command = message.content.replace(" ", "")
+        #print(command)
 
-        if command in ['오버', '수치']:
-            print(message.author, command)
-            if command == '오버':
-                answer = over(message.author)
-                await message.channel.send(answer)
-            elif command == '수치':
-                answer = cali(message.author)
-                await message.channel.send(answer)
- 
+        command = message.content
+        ret = False
+        for c in ['오버', '수치', '잠금', '해제', '비틱', '관리']:
+            if c in command:
+                ret = True
+
+        if ret == False:
+            return
+
+        print(message.author.name, message.author.nick, command)
+        if message.author.name not in dAccount:
+            account = over.Account(message.author.nick if message.author.nick != None else message.author.name)
+            dAccount[message.author.name] = account
+        else:
+            account = dAccount[message.author.name]
+
+        cmd = re.sub(r'\s+', ' ', message.content.strip())
+        cmdList = cmd.split(' ')
+        print(cmdList)
+        cmd = cmdList[0]
+        
+        answer = ''
+        if len(cmdList) == 1:
+            if cmd == '오버':
+                answer = account.over()
+            elif cmd == '수치':
+                answer = account.over(True)
+        elif len(cmdList) == 2:
+            if cmd == '오버' and cmdList[1] in over.NIKKES:
+                answer = account.dNikke[cmdList[1]].info()
+            elif cmd == '잠금' and cmdList[1].isdigit():
+                answer = account.lock(int(cmdList[1]))
+            elif cmd == '해제' and cmdList[1].isdigit():
+                answer = account.unlock(int(cmdList[1]))
+            elif cmd == '비틱' and cmdList[1] in over.NIKKES:
+                if len(dAccount) > 0:
+                    topAccount = getTopAccount(cmdList[1])
+                    answer = '비틱 1위 계정 : %s \n'%(topAccount.nick)
+                    answer += topAccount.dNikke[cmdList[1]].info()
+                else:
+                    answer = '아직 아무도 오버를 안했습니다.'
+        elif len(cmdList) == 3:
+            if cmd == '오버' and cmdList[1] in over.NIKKES and cmdList[2] in over.PIECES:
+                answer = account.changeCurNikkePiece(cmdList[1], cmdList[2])
+            elif cmd == '관리' and cmdList[1] == '리젠' and cmdList[2].isdigit() and message.author.name == 'murloc1217':
+                over.RECHARGE_SECONDS = int(cmdList[2])
+
+        if answer != '':
+            await message.channel.send(answer)
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
---
+++ over.py
@@ -0,0 +1,316 @@
+from datetime import datetime
+import random
+
+table = {'우월': [10, 1,  10, [9.54,  10.94, 12.34, 13.75, 15.15, 16.55, 17.95, 19.35, 20.75, 22.15, 23.56, 24.96, 26.36, 27.76, 29.16]],
+         '명중': [12, 11, 22, [4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]],
+         '장탄': [12, 23, 34, [27.84, 31.95, 36.06, 40.17, 44.28, 48.39, 52.50, 56.50, 60.71, 64.82, 68.93, 73.04, 77.15, 81.26, 85.37]],
+         '공증': [10, 35, 44, [4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]],
+         '차뎀': [12, 45, 56, [4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]],
+         '차속': [12, 57, 68, [1.98,  2.28,  2.57,  2.86,  3.16,  3.45,  3.75,  4.04,  4.33,  4.63,  4.92,  5.21,  5.51,  5.80,  6.09 ]],
+         '크뎀': [12, 69, 80, [6.64,  7.62,  8.60,  9.58,  10.56, 11.54, 12.52, 13.50, 14.48, 15.46, 16.44, 17.42, 18.40, 19.38, 20.36]],
+         '크확': [10, 81, 90, [2.30,  2.64,  2.98,  3.32,  3.66,  4.00,  4.35,  4.69,  5.03,  5.37,  5.70,  6.05,  6.39,  6.73,  7.07 ]],
+         '방어': [10, 91, 100,[4.77,  5.47,  6.18,  6.88,  7.59,  8.29,  9.00,  9.70,  10.40, 11.11, 11.81, 12.52, 13.22, 13.93, 14.63]]}
+         
+
+abbr = {'홍': '횽련', '모': '모더니아', '앨': '앨리스', '백': '백설', '맥':'맥스웰', '볼':'볼륨',
+        '뚝': '뚝배기', '갑': '갑빠', '장': '장갑', '신': '신발',
+        '우월': '우월코드 데미지 증가', '명중': '명중률 증가', '장탄': '최대 장탄수 증가', '공증': '공격력 증가', '차뎀': '차지 데미지 증가', '차속': '차지 속도 증가', 
+        '크뎀': '크리티컬 데미지 증가', '크확': '크리티컬 확률 증가', '방어': '방어력 증가'
+}
+
+effective = {'홍' : ['우월','공증', '장탄', '명중'],
+             '모' : ['우월','공증', '장탄', '크확'],
+             '앨' : ['우월','공증', '장탄', '차속'],
+             '백' : ['우월','공증', '크뎀', '차속'],
+             '맥' : ['우월','공증', '장탄', '차뎀'],
+             '볼' : ['우월','공증', '장탄', '명중'],
+}
+
+NIKKES = ['홍', '모', '앨', '맥', '백', '볼']
+PIECES = ['뚝', '갑', '장', '신']
+
+def clamp(num, min, max):
+    return min if num < min else max if num > max else num
+
+MODULE_MAX = 10
+RECHARGE_SECONDS = 60
+#RECHARGE_SECONDS = 5
+
+class Option():
+    def __init__(self, prob):
+        self.prob = prob
+        self.reset()
+
+    def reset(self):
+        self.lock = False
+        self.effect = '효과 미획득'
+        self.rate = 0.0
+        self.idx  = 0
+
+    def desc(self):
+        out = ''
+        if self.effect == '효과 미획득':
+            out = '효과 미획득'
+        else:
+            out = '[%s] %.2f'%(abbr[self.effect], self.rate)+'%'+' (%dLv)'%(self.idx+1)
+            if self.lock:
+                out = out + ' #'
+
+        return out+'\n'
+
+    def score(self, nikke):
+        if self.effect in effective[nikke]:
+            return 15 + self.idx+1
+        else:
+            return 0
+
+class Piece():
+    def __init__(self, name):
+        self.name = name
+        self.options = []
+        
+        for p in [100, 50, 30]:
+            option = Option(p)
+            self.options.append(option)
+
+    def over(self):
+        for o in self.options:
+            if o.lock == False:
+                o.reset()
+
+        for o in self.options:
+            if o.lock == False:
+                ret, o.effect  = self.roleOption(o.prob)
+                if ret:
+                    o.idx   = self.roleRatio()
+                    o.rate  = table[o.effect][3][o.idx]
+
+    def cali(self):
+        for o in self.options:
+            if o.lock == False and o.effect != '효과 미획득':
+                o.idx   = self.roleRatio()
+                o.rate  = table[o.effect][3][o.idx]
+
+    def desc(self):
+        out = ''
+        for o in self.options:        
+            out += o.desc()
+
+        return out
+
+    def alreadyHas(self, effect):
+        for o in self.options:
+            if o.effect == effect:
+                return True
+
+        return False
+    
+    def roleRatio(self):
+        howProb = random.randint(0, 99)
+        getIdx = 0
+        if howProb < 60:
+            getIdx = int(howProb/12)
+        elif howProb < 95:
+            getIdx = 5+int((howProb-60)/7)
+        else:
+            getIdx = 10+howProb-95
+
+        return getIdx
+    
+    def roleOption(self, prob):
+        if random.randint(1, 100) <= prob:
+            while True:
+                whichProb = random.randint(1, 100)
+                for effect, v in table.items():
+                    if whichProb >= v[1] and whichProb <= v[2]:
+                        break
+
+                if self.alreadyHas(effect) == False:
+                    break
+
+            return True, effect
+        else:
+            return False, '효과 미획득'
+    
+    def getLockedCount(self):
+        ret = 0
+        for o in self.options:
+            if o.lock:
+                ret += 1
+
+        return ret
+
+    def calcOverNeedModule(self):
+        lockCount = self.getLockedCount()
+        if lockCount == 1:
+            return 2
+        elif lockCount == 2:
+            return 3
+        else:
+            return 1
+
+    def calcLockNeedModule(self):
+        lockCount = self.getLockedCount()
+        if lockCount == 1:
+            return 3
+        else:
+            return 2
+
+    def unlock(self, index):
+        self.options[index].lock = False
+
+    def unlockEnable(self, index):
+        if index < 0 or index > 2:
+            return False, '1,2,3 만 해제 가능'
+
+        return True, ''
+        
+    def lock(self, index):
+        lockCount = self.getLockedCount()
+        if lockCount >= 2:
+            return False
+        else:
+            self.options[index].lock = True
+
+    def lockEnable(self, index):
+        lockCount = self.getLockedCount()
+        if lockCount >= 2:
+            return False, '2부위 이상 잠금 불가'
+        elif index < 0 or index > 2:
+            return False, '1,2,3 만 잠금 가능'
+        elif self.options[index].effect == '효과 미획득':
+            return False, '효과 미획득은 잠금 불가'
+
+        return True, ''
+
+    def score(self, nikke):
+        s = 0
+        for o in self.options:
+            s += o.score(nikke)
+        
+        return s
+
+class Nikke():
+    def __init__(self, name):
+        self.usedModule = 0
+        self.name = name
+        self.dPiece = {}
+        for n in PIECES:
+            self.dPiece[n] = Piece(n)
+
+        self.curPiece = self.dPiece['뚝']
+
+    def useModule(self, moduleCount):
+        self.usedModule += moduleCount
+
+    def desc(self):
+        return '%s에 사용한 모듈수: %d \n'%(abbr[self.name], self.usedModule)
+
+    def info(self):
+        out = ''
+        out += self.desc()
+
+        for n, p in self.dPiece.items():
+            out += '<< %s >>'%abbr[n] + '\n'
+            out += p.desc()
+
+        return out
+
+    def score(self):
+        s = 0
+        for n, p in self.dPiece.items():
+            s += p.score(self.name)
+
+        return s
+
+class Account():
+    def __init__(self, nick):
+        self.module = MODULE_MAX
+        self.lastAccess = datetime.now()
+        self.nick = nick
+
+        self.dNikke = {}
+        for n in NIKKES:
+            self.dNikke[n] = Nikke(n)
+
+        self.curNikke = self.dNikke['홍']
+
+    def rechargeModule(self):
+        self.module = clamp(self.module + int((datetime.now() - self.lastAccess).seconds/RECHARGE_SECONDS), 0, MODULE_MAX)
+
+    def processModulel(self, needModule):
+        self.rechargeModule()
+        
+        if self.module >= needModule:
+            self.lastAccess = datetime.now()
+            self.module = self.module - needModule
+            self.curNikke.useModule(needModule)            
+            return True, 0
+        else:
+            return False, RECHARGE_SECONDS-(datetime.now() - self.lastAccess).seconds
+        
+    def desc(self):
+        return '남은 커스텀 모듈 갯수: %d'%(self.module)
+
+    def over(self, isCali=False):
+        needModule = self.curNikke.curPiece.calcOverNeedModule()
+        success, timeToCharge = self.processModulel(needModule)
+        out = ''
+        
+        if success:
+            if isCali:
+                self.curNikke.curPiece.cali()
+            else:
+                self.curNikke.curPiece.over()
+
+            out += '%s %s 오버 성공 \n'%(abbr[self.curNikke.name], abbr[self.curNikke.curPiece.name])
+            out += self.curNikke.desc()
+            out += self.curNikke.curPiece.desc()
+            out += self.desc()
+            return out
+        else:
+            return '커스텀 모듈 갯수가 부족합니다.\n커스텀 모듈은 %d초에 1개씩 충전됩니다. 남은시간 %d초'%(RECHARGE_SECONDS, timeToCharge)
+
+    def lock(self, index):
+        index -= 1
+        success, msg = self.curNikke.curPiece.lockEnable(index)
+        if success == False:
+            return msg
+
+        needModule = self.curNikke.curPiece.calcLockNeedModule()
+        success, timeToCharge = self.processModulel(needModule)
+        out = ''
+        
+        if success:
+            self.curNikke.curPiece.lock(index)
+            out += '잠금 성공\n'
+            out += self.curNikke.desc()
+            out += self.curNikke.curPiece.desc()
+            out += self.desc()
+            return out
+        else:
+            return '커스텀 모듈 갯수가 부족합니다.\n커스텀 모듈은 1분에 1개씩 충전됩니다. 남은시간 %d초'%(timeToCharge)
+
+    def unlock(self, index):
+        index -= 1
+
+        success, msg = self.curNikke.curPiece.unlockEnable(index)
+        if success == False:
+            return msg
+
+        self.curNikke.curPiece.unlock(index)
+        out = ''
+        out += '잠금 해제 성공\n'
+        out += self.curNikke.curPiece.desc()
+        
+        return out
+
+    def changeCurNikkePiece(self, nikke, piece):
+        self.curNikke = self.dNikke[nikke]
+        self.curNikke.curPiece = self.curNikke.dPiece[piece]
+        out = ''
+        out += ' %s %s로 오버할 장비 변경\n'%(abbr[nikke], abbr[piece])
+        out += self.curNikke.desc()
+        out += self.curNikke.curPiece.desc()
+
+        return out
